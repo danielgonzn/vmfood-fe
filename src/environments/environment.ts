b4ed32c0729ef22f5b1c748bc7818e16529bc03a
Daniel Gonzalez <ing.danielgonzn@gmail.com>
2026-03-18 22:39:23 -0400

    chore: configurar dominios de produccion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://127.0.0.1:8000/api/v1',
-  backendBaseUrl: 'http://127.0.0.1:8000',
+  apiBaseUrl: 'https://panel.vmfoodimport.com/api/v1',
+  backendBaseUrl: 'https://panel.vmfoodimport.com',
 };

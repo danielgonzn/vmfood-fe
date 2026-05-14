5626ea769a14249339c6f90d3cf0728069f33992
Daniel <dgonzalez@impruvex.com>
2026-05-14 17:15:06 -0400

    Despliegue de producción

--- dist/app/server/angular-app-manifest.mjs
+++ dist/app/server/angular-app-manifest.mjs
@@ -33,12 +33,12 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 31989, hash: '5af8f4686a3e5ff7f80e3163ece062151dd6cd7d1fd63805532368d9b9d317aa', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 2221, hash: 'f312a7a2c069d663dcfc21364f12e7481d5cbe4307c1aad4596eb50fe64959d9', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'terminos-y-condiciones/index.html': {size: 51148, hash: '83b408a540869a1a39e9b1fa845aad5d211811404684137355921ff0b3e77cff', text: () => import('./assets-chunks/terminos-y-condiciones_index_html.mjs').then(m => m.default)},
-    'aviso-de-privacidad/index.html': {size: 51195, hash: 'e31da0fc6a734d769e5aa923d1829ca624223a84c87376c08bc4e762593ab038', text: () => import('./assets-chunks/aviso-de-privacidad_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 106107, hash: 'a0bf762e569e1cf6c5fcaf29f35e010eba9acb2d2632173fd140bb8a09b558f1', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'catalogo/index.html': {size: 98018, hash: 'ce01eada22ffb275f596ee6586db14f64e0b2f7486e0ea2cbc3d03f7be532959', text: () => import('./assets-chunks/catalogo_index_html.mjs').then(m => m.default)},
-    'styles-UPTMFLEH.css': {size: 71189, hash: 'XD5EG44TwNI', text: () => import('./assets-chunks/styles-UPTMFLEH_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 32095, hash: '393b559e44ccb1b26af992628b648ad839dada60ad7d1c652f6e35de907a980d', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 2221, hash: '9bbf0ea81198a6042b928ac71ea1328a404667101953344256b3bd6999492db2', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'terminos-y-condiciones/index.html': {size: 51259, hash: '69065af9b996f0b71ec67cb55a2ae833b4e08a0859beeae5cc30aea2dc45963f', text: () => import('./assets-chunks/terminos-y-condiciones_index_html.mjs').then(m => m.default)},
+    'aviso-de-privacidad/index.html': {size: 51306, hash: '704f0a5f92e73828ccb6c56ead7ec8e22d0467517ca2f363079e38527f8210a7', text: () => import('./assets-chunks/aviso-de-privacidad_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 106254, hash: '42a55135eba99fb543945418a89746a2aea3ad3e352081e6631cab310c000658', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'catalogo/index.html': {size: 98129, hash: '5938378236175e3a9621510a4da26d1a350046891694eb1b51eddc4102f39bb5', text: () => import('./assets-chunks/catalogo_index_html.mjs').then(m => m.default)},
+    'styles-F2HCRSD5.css': {size: 74026, hash: 'lHI+gwPS7Yo', text: () => import('./assets-chunks/styles-F2HCRSD5_css.mjs').then(m => m.default)}
   },
 };
